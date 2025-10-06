18422950e0397f4e37ff4790052c532800245095
Vedant-Nagarkar <vedantnagarkar01@gmail.com>
2025-10-06 23:18:45 +0530

    model trainer

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from TextSummerizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from TextSummerizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from TextSummerizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from TextSummerizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from TextSummerizer.logging import logger
-
+ 
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -33,6 +34,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_trasformation = DataTransformationTrainingPipeline()
    data_trasformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
---
+++ src/TextSummerizer/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,17 @@
+from TextSummerizer.config.configuration import ConfigurationManager
+from TextSummerizer.components.model_trainer import ModelTrainer
+from TextSummerizer.logging import logger
+
+
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+         pass 
+    
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer = ModelTrainer(config=model_trainer_config)  # ✅ use different variable name
+        model_trainer.train()
+        
+
